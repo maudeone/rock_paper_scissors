2a804d771de5227da45272c47eeb9eb6bea24058
Maude <ayoubbb973@gmail.com>
2021-05-07 13:59:36 -0400

    Added basic style.css and archived html/js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 let playerScore = 0;
-    let compScore = 0;  
+let compScore = 0;
+
+//naming user buttons to add event listeners
+const optionBtn = document.querySelectorAll('.optionBtn');
+const playerPoints = document.querySelector('#playerScore');
+const computerPoints = document.querySelector('#compScore');
+const resetBtn = document.querySelector('#reset');
+
+resetBtn.addEventListener('click',() => location.reload());
+optionBtn.forEach(button => { button.addEventListener('click', getPlayerChoice) });
+
+function getPlayerChoice(e) {
+  playerChoice = e.target.textContent;
+  playRound(playerChoice, );
+}
 
     function computerPlay() {
     let choices = ["Rock", "Paper", "Scissor"];
@@ -7,7 +21,6 @@ let playerScore = 0;
     }
   
     function playRound(playerChoice, compChoice) {
-    playerChoice = prompt("Rock, Paper, or Scissor", "Rock");
     console.log("You choose ", playerChoice);
     compChoice = computerPlay();
     console.log("Computer choose ", compChoice);  
@@ -15,32 +28,42 @@ let playerScore = 0;
       playerScore = playerScore;
       compScore = compScore;
       console.log("Tie!", playerScore, compScore);
+      playerPoints.textContent = playerScore;
+      computerPoints.textContent = compScore;
+      roundResults.textContent = "Tied this round!";
+
    
     } else if (
       (playerChoice == "Rock" && compChoice == "Scissor") || (playerChoice == "Paper" && compChoice == "Rock") || (playerChoice == "Scissor" && compChoice == "Paper")
       ) {   
         playerScore = ++playerScore;
         console.log("You win round", playerScore, compScore);
-        
+        playerPoints.textContent = playerScore;
+        roundResults.textContent = "You Win round!";
     
     } else if (
       (playerChoice == "Rock" && compChoice == "Paper") || (playerChoice == "Paper" && compChoice == "Scissor") || (playerChoice == "Scissor" && compChoice == "Rock")
       ) {
         compScore = ++compScore;
         console.log("You lose round ", playerScore, compScore);   
-          
+        computerPoints.textContent = compScore;
+        roundResults.textContent = "You Lose round!";  
     }
     gameCheck();
     }
-    function gameCheck() {
-    if (compScore < 5 && playerScore < 5) {
-      playRound();
+function gameCheck() {
+  if (compScore < 5 && playerScore < 5) {
 
-    } else if (playerScore == 5) {
-      console.log('You win!');
+  } else if (playerScore >= 5) {
+    console.log('You win Game!');
+    roundResults.textContent = "You Win Game!";  
+    optionBtn.forEach(button => { button.removeEventListener('click', getPlayerChoice);
+  });
 
-    } else if (compScore == 5) {
-      console.log('Computer wins.');
-    }
-    }
-    console.log(gameCheck());
+  } else if (compScore >= 5) {
+    console.log('Computer wins Game.');
+    roundResults.textContent = "You Lose Game!";  
+    optionBtn.forEach(button => { button.removeEventListener('click', getPlayerChoice);
+  });
+  }
+}
